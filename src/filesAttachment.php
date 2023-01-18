1422e5772138bb4647151ba9183930bc3b5430a3
MostafaSewidan <sewidanmostafa@gmail.com>
2023-01-18 17:38:54 +0200

    edits

--- src/filesAttachment.php
+++ src/filesAttachment.php
@@ -2,54 +2,64 @@
 
 use Carbon\Carbon;
 use Illuminate\Support\Facades\File;
-/**
- * @param $file
- * @param $folder_name
- * @return string
- */
-function addAttachment($file, $folder_name)
-{
 
-    ///////////////////////////////
+if (!function_exists('addAttachment')) {
 
-    $destinationPath = public_path().'/uploads/'.$folder_name.'/';
-    $extension = $file->getClientOriginalExtension(); // getting image extension
+    /**
+     * @param $file
+     * @param $folder_name
+     * @return string
+     */
+    function addAttachment($file, $folder_name)
+    {
+        ///////////////////////////////
 
-    if (!File::isDirectory($destinationPath)) {
-        File::makeDirectory($destinationPath, 0755, true, true);
+        $destinationPath = public_path().'/uploads/'.$folder_name.'/';
+        $extension = $file->getClientOriginalExtension(); // getting image extension
+
+        if (!File::isDirectory($destinationPath)) {
+            File::makeDirectory($destinationPath, 0755, true, true);
+        }
+
+        $image = '-'.time().''.rand(11111, 99999).'.'.$extension;
+        $file->move($destinationPath, $image); // uploading file to given
+        $path = 'uploads/'.$folder_name.'/'.$image;
+
+        return $path;
     }
-
-    $image = '-'.time().''.rand(11111, 99999).'.'.$extension;
-    $file->move($destinationPath, $image); // uploading file to given
-    $path = 'uploads/'.$folder_name.'/'.$image;
-
-    return $path;
 }
 
-/**
- * @param $file
- * @param $oldFilesPath
- * @param $folder_name
- * @return string
- */
-function updateAttachment(
-    $file,
-    $oldFilesPath,
-    $folder_name
-) {
-    deleteAttachment($oldFilesPath);
-    addAttachment($file, $folder_name);
-}
+if (!function_exists('updateAttachment')) {
 
-/**
- * @param $file_path
- * @return bool
- */
-function deleteAttachment($file_path)
-{
-    foreach ((array) $file_path as $file) {
-
-        File::delete(public_path().'/'.$file);
+    /**
+     * @param $file
+     * @param $oldFilesPath
+     * @param $folder_name
+     * @return string
+     */
+    function updateAttachment(
+        $file,
+        $oldFilesPath,
+        $folder_name
+    ) {
+        deleteAttachment($oldFilesPath);
+        addAttachment($file, $folder_name);
     }
-    return true;
 }
+
+if (!function_exists('deleteAttachment')) {
+
+    /**
+     * @param $file_path
+     * @return bool
+     */
+    function deleteAttachment($file_path)
+    {
+        foreach ((array) $file_path as $file) {
+
+            File::delete(public_path().'/'.$file);
+        }
+        return true;
+    }
+}
+
